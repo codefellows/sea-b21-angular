d61e7c53fef047d2767f859f2ce3a3c36490d478
toastynerd <tylermorgan86@gmail.com>
2014-09-30 16:02:02 -0700

    add basic controller

--- app/js/client.js
+++ app/js/client.js
@@ -1,1 +1,6 @@
-alert('hello world!');
+require('angular/angular');
+
+var demoApp = angular.module('demoApp', []);
+demoApp.controller('firstController', function($scope) {
+  $scope.greeting = 'Hello World';
+});
