18bce3f3d838308e1dba6935ea56287211859848
toastynerd <tylermorgan86@gmail.com>
2014-09-30 14:25:28 -0700

    starting point for ui/ux class

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.initConfig({
+    clean: {
+      dev: {
+        src: ['build/']
+      }
+    },
+
+    copy: {
+      dev: {
+        expand: true,
+        cwd: 'app/',
+        src: ['*.html', '*.css'],
+        dest: 'build/',
+        filter: 'isFile'
+      }
+    },
+
+    browserify: {
+      dev: {
+        options: {
+          transform: ['debowerify'],
+          debug: true
+        },
+        src: ['app/js/**/*.js']
+        dest: 'build/bundle.js'
+      }
+    }
+  });
+
+  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev'])
+}
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+var server = http.createServer(app);
+server.listen(3000, function() {
+  console.log('server is operational, commence dynamic synergy solutions.');
+});
